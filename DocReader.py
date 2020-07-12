bcaa0c246e5c9b17c87fbc63eda445f886221a37
GJuniorG <49473084+GJuniorG@users.noreply.github.com>
2020-07-12 13:18:08 +0200

    Update DocReader.py
    dealing with multiple runs

--- DocReader.py
+++ DocReader.py
@@ -14,7 +14,7 @@ def translat(src_text):
     time1 = time.time()
     translated = model.generate(**tokenizer.prepare_translation_batch(src_text))
     tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
-    print("\t " + "OUTPUT ::: " + str(tgt_text))
+    print("\t " + "TRANSLATION ::: " + str(tgt_text))
     time2 = time.time()
     duration = (time2 - time1) * 1000.0
     return tgt_text, duration
@@ -38,16 +38,26 @@ def translateDocx(source, target, file):
     doc = Document(file)
     for p in doc.paragraphs:
         inline = p.runs
+        print("\t " + "No. of RUNS for this para ::: " +str(len(inline)))
         for i in range(len(inline)):
             text = inline[i].text
             textsL = [text]
             textsL = filter(lambda x: not re.match(r'^\s*$', x), textsL)
             for text_value in textsL:
                 textElem = [text_value]
-                translationList, duration = translat(textElem)
-                for translatedE in translationList:
-                    text = text.replace(text_value, translatedE)
-                    inline[i].text = text
+                if len(inline) > 1:
+                    if len(text_value).__eq__(1):
+                        inline[i].text = text_value
+                    else:
+                        translationList, duration = translat(textElem)
+                        for translatedE in translationList:
+                            text = text.replace(text_value, translatedE)
+                            inline[i].text = text + " "
+                else:
+                    translationList, duration = translat(textElem)
+                    for translatedE in translationList:
+                        text = text.replace(text_value, translatedE)
+                        inline[i].text = text
 
     end_time = time.time()
     duration = (start_time - end_time) * 1000.0
